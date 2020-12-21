e570c2513b5e281b10bf644879d504b93cfd7228
QiJing Chen <qijing@wo.cn>
2020-12-21 11:52:15 +0800

    添加 flutter_screenutil

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-# flutter_template_app
+主页
+    - 底部栏切换
 
-A new Flutter project.
+列表页
+    - 列表页项
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+个人中心
+    - 个人信息
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil_init.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      designSize: Size(320.0, 667.0),
+      allowFontScaling: false,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        // routes: {"new_page": (context) => NewRoute()},
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
